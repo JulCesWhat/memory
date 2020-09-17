2bdc5b954ba40534c6efdecf285bd618183693e7
Whatley, Cesar <julceswhat@gmail.com>
2020-09-17 19:23:46 -0400

    Creating basic card views for game

--- memory/ContentView.swift
+++ memory/ContentView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            ForEach(0..<4) { index in
+                CardView()
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+    }
+}
+
+struct CardView: View {
+    var isFacedUp = true
+    var body: some View {
+        ZStack {
+            if isFacedUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3.0)
+                Text("👻")
+                
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
     }
 }
 
@@ -19,3 +40,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
